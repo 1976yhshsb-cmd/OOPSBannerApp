c994dff9599c3fe9079ed30a02872c87d1f86205
1976.yhshsb-cmd <1976.yhshsb@gmail.com>
2026-02-24 14:07:28 +0530

    Refractored Banner Logic into Static Functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,65 +1,69 @@
 /**
- * OOPSBannerApp UC4 – Render OOPS as Banner using String Array and Loop
+ * OOPSBannerApp UC6 - Refactor Banner Logic into Static Functions
  *
- * This program stores each line of the OOPS banner in a String array
- * using String.join() and prints them using a for-each loop.
+ * This use case extends UC5 by creating helper methods
+ * for each character pattern (O, P, S).
  *
  * @author Developer
- * @version 4.0
+ * @version 6.0
  */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Create String array with 7 lines
         String[] lines = new String[7];
 
-        // Populate each line using String.join()
-        lines[0] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                " ******  ",
-                " ******  ");
+        for (int i = 0; i < 7; i++) {
+            lines[i] = String.join(" ",
+                    buildO()[i],
+                    buildO()[i],
+                    buildP()[i],
+                    buildS()[i]
+            );
+        }
 
-        lines[1] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *     * ",
-                " *       ");
-
-        lines[2] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " ******  ",
-                " *****   ");
-
-        lines[3] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "      *  ");
-
-        lines[4] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                "      *  ");
-
-        lines[5] = String.join(" ",
-                " *     * ",
-                " *     * ",
-                " *       ",
-                " *    *  ");
-
-        lines[6] = String.join(" ",
-                "  *****  ",
-                "  *****  ",
-                " *       ",
-                " *****   ");
-
-        // Use for-each loop to print banner
         for (String line : lines) {
             System.out.println(line);
         }
+    }
+
+    // Helper method for O
+    public static String[] buildO() {
+        return new String[]{
+                " ***  ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                "*   * ",
+                " ***  "
+        };
+    }
+
+    // Helper method for P
+    public static String[] buildP() {
+        return new String[]{
+                "***** ",
+                "*   * ",
+                "*   * ",
+                "***** ",
+                "*     ",
+                "*     ",
+                "*     "
+        };
+    }
+
+    // Helper method for S
+    public static String[] buildS() {
+        return new String[]{
+                " **** ",
+                "*     ",
+                "*     ",
+                " ***  ",
+                "     *",
+                "     *",
+                "****  "
+        };
     }
 }
